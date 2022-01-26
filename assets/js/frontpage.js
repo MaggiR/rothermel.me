08d8e53e1aafd5c0658ddf33466868c75788381c
MaggiR <mark.rothermel@web.de>
2022-01-27 00:14:38 +0100

    Cleanup code

--- assets/js/frontpage.js
+++ assets/js/frontpage.js
@@ -2,7 +2,6 @@ mainScreen = document.getElementById("main-screen")
 
 function updateContentHeight() {
   mainScreen.style.minHeight = (window.innerHeight - 1).toString() + "px";
-  console.log("Height updated")
 }
 
 updateContentHeight();
